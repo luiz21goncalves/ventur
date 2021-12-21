8de637c6141a30d7cccb42054c1773b371c84762
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-12-21 18:14:08 -0300

    feat(react): add home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-
-import { Greetings } from './components/Greetings'
 import { ChakraProvider } from '@chakra-ui/react'
+
 import { theme } from './styles/theme'
+import { Home } from './screens/Home'
 
 export function App () {
   return (
     <ChakraProvider theme={theme}>
-      <Greetings />
+      <Home />
     </ChakraProvider>
   )
 }
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Flex, Heading } from '@chakra-ui/react'
+
+export function Home () {
+  return (
+    <Flex w="full" h="100vh" align="center" justify="center" direction="column" >
+      <Heading>Home</Heading>
+    </Flex>
+  )
+}
