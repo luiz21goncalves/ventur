fdd804924af20849a90cc31438738b7f75f6faf5
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-12-22 14:49:35 -0300

    feat(react): add routes to student create & student list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,16 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from '../screens/Home';
+import { StudentCreate } from '../screens/Student/Create';
+import { StudentList } from '../screens/Student/List';
 
 export function AppRoutes() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/students" element={<StudentList />} />
+        <Route path="/students/create" element={<StudentCreate />} />
       </Routes>
     </HashRouter>
   );
