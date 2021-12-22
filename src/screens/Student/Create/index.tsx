1afb60722ce8462203897b816e7d16b2cbdb52e2
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-12-22 15:32:24 -0300

    chore(react): add react hook form

--- src/screens/Student/Create/index.tsx
+++ src/screens/Student/Create/index.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   Input,
@@ -14,65 +15,89 @@ import {
   Stack,
 } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import { BaseScreen } from '../../../components/BaseScreen';
 
 export function StudentCreate() {
   const navigate = useNavigate();
 
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data: unkonwn) {
+    console.log(data);
+    reset();
+  }
+
   return (
     <BaseScreen>
       <Box width="container.sm">
         <Heading textAlign="center">Novo aluno</Heading>
 
-        <Flex width="full" justifyContent="space-between" my="8">
-          <Button
-            width="40"
-            colorScheme="red"
-            variant="outline"
-            onClick={() => navigate(-1)}
-          >
-            Voltar
-          </Button>
+        <Stack as="form" spacing="4" onSubmit={handleSubmit(onSubmit)}>
+          <Flex width="full" justifyContent="space-between" my="8">
+            <Button
+              width="40"
+              colorScheme="red"
+              variant="outline"
+              onClick={() => navigate(-1)}
+            >
+              Voltar
+            </Button>
 
-          <Button width="40" colorScheme="green" type="submit">
-            Salvar
-          </Button>
-        </Flex>
+            <Button width="40" colorScheme="green" type="submit">
+              Salvar
+            </Button>
+          </Flex>
 
-        <Stack as="form" spacing="4">
           <FormControl isRequired>
             <FormLabel htmlFor="name">Nome</FormLabel>
-            <Input id="name" type="email" />
+            <Input id="name" {...register('name', { required: true })} />
+            <FormErrorMessage>{errors.name}</FormErrorMessage>
           </FormControl>
 
           <FormControl>
             <FormLabel htmlFor="email">Email</FormLabel>
-            <Input id="email" />
+            <Input id="email" type="email" {...register('email')} />
+            <FormErrorMessage>{errors.email}</FormErrorMessage>
           </FormControl>
 
           <FormControl>
             <FormLabel htmlFor="password">Senha</FormLabel>
-            <Input id="password" />
+            <Input id="password" {...register('password')} />
+            <FormErrorMessage>{errors.password}</FormErrorMessage>
           </FormControl>
 
           <Stack direction="row" spacing="8">
             <FormControl isRequired>
               <FormLabel htmlFor="classes_per_week">Aulas por semana</FormLabel>
               <NumberInput defaultValue={0} min={0} max={7}>
-                <NumberInputField id="classes_per_week" />
+                <NumberInputField
+                  id="classes_per_week"
+                  {...register('classes_per_week', { valueAsNumber: true })}
+                />
                 <NumberInputStepper>
                   <NumberIncrementStepper />
                   <NumberDecrementStepper />
                 </NumberInputStepper>
               </NumberInput>
+              <FormErrorMessage>{errors.classes_per_week}</FormErrorMessage>
             </FormControl>
 
             <FormControl isRequired>
               <FormLabel htmlFor="price_per_month">Preço por mês</FormLabel>
               <NumberInput defaultValue={0} precision={2}>
-                <NumberInputField id="price_per_month" />
+                <NumberInputField
+                  id="price_per_month"
+                  {...register('price_per_month', { valueAsNumber: true })}
+                />
               </NumberInput>
+              <FormErrorMessage>{errors.price_per_month}</FormErrorMessage>
             </FormControl>
           </Stack>
         </Stack>
