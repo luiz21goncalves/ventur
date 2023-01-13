7f231375cf9502d81118c6e30c9c7543702f69d0
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-13 16:10:17 -0300

    feat: add toggle theme button

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,13 @@
 import { ChakraProvider, Heading } from '@chakra-ui/react'
 
+import { ToggleTheme } from './components/ToggleTheme'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={defaultTheme}>
       <Heading>Ventur</Heading>
+      <ToggleTheme />
     </ChakraProvider>
   )
 }
