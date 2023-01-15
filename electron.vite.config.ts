ac2924ae00cabab7e4678cd0ef112d5b6c8adc8f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-15 10:33:00 -0300

    chore: add jotai config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,8 @@ import { resolve } from 'node:path'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import jotaiPluginDebugLabel from 'jotai/babel/plugin-debug-label'
+import jotaiPluginReactRefresh from 'jotai/babel/plugin-react-refresh'
 
 export default defineConfig({
   main: {
@@ -11,7 +13,11 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()],
   },
   renderer: {
-    plugins: [react()],
+    plugins: [
+      react({
+        babel: { plugins: [jotaiPluginDebugLabel, jotaiPluginReactRefresh] },
+      }),
+    ],
     resolve: {
       alias: {
         '@': resolve('src/'),
