8098e61c748128703f9203cbe0f35bbcbc6ca444
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-21 12:49:32 -0300

    feat: add navigation

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,14 +2,14 @@ import './lib/dayjs'
 
 import { Box, ChakraProvider, SystemStyleObject } from '@chakra-ui/react'
 
-import { Home } from './screens/Home'
+import { Routes } from './Routes'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ChakraProvider theme={defaultTheme}>
       <Box w="100vw" h="100vh" bgColor="gray.50" _dark={darkStyles}>
-        <Home />
+        <Routes />
       </Box>
     </ChakraProvider>
   )
