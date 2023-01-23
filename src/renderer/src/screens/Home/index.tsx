7ab273ede76fb888cb0a6138adabfd787e2cee23
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-23 20:24:51 -0300

    refactor: apply custom scrollbar globally

--- src/renderer/src/screens/Home/index.tsx
+++ src/renderer/src/screens/Home/index.tsx
@@ -8,7 +8,6 @@ import {
   GridItem,
   Heading,
   Text,
-  useColorModeValue,
   useToken,
   VStack,
 } from '@chakra-ui/react'
@@ -27,17 +26,6 @@ export function Home() {
     'container.sm',
   ])
 
-  const scrollbarBackgroundColor = useColorModeValue(
-    'blackAlpha.50',
-    'whiteAlpha.50',
-  )
-  const scrollbarColor = useColorModeValue('blackAlpha.200', 'whiteAlpha.200')
-
-  const scrollbarStyles = getScrollbarStyles({
-    scrollbarBackgroundColor,
-    scrollbarColor,
-  })
-
   function handleNavigateToStudentDetails(studentId: string) {
     navigate(`/student/${studentId}`)
   }
@@ -48,7 +36,7 @@ export function Home() {
       gap="4"
       h="container.md"
     >
-      <GridItem overflowY="auto" pr="4" py="4" sx={scrollbarStyles}>
+      <GridItem overflowY="auto" pr="4" py="4">
         <VStack gap="4" align="flex-start">
           {students.map((student) => {
             function handleRedirect() {
@@ -84,24 +72,4 @@ export function Home() {
       </GridItem>
     </Grid>
   )
-}
-
-const getScrollbarStyles = ({
-  scrollbarBackgroundColor,
-  scrollbarColor,
-}: {
-  scrollbarBackgroundColor: string
-  scrollbarColor: string
-}) => {
-  return {
-    '&::-webkit-scrollbar': {
-      backgroundColor: `${scrollbarBackgroundColor}`,
-      borderRadius: '6px',
-      width: '6px',
-    },
-    '&::-webkit-scrollbar-thumb': {
-      backgroundColor: `${scrollbarColor}`,
-      borderRadius: '6px',
-    },
-  }
 }
--- src/renderer/src/styles/themes/default.ts
+++ src/renderer/src/styles/themes/default.ts
@@ -1,8 +1,25 @@
-import { extendTheme, ThemeConfig } from '@chakra-ui/react'
-
+import { extendTheme, StyleFunctionProps, ThemeConfig } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
 const config: ThemeConfig = {
   initialColorMode: 'system',
   useSystemColorMode: true,
 }
 
-export const defaultTheme = extendTheme({ config })
+export const defaultTheme = extendTheme({
+  config,
+  styles: {
+    global: (props: StyleFunctionProps) => ({
+      '*': {
+        '&::-webkit-scrollbar': {
+          backgroundColor: mode('blackAlpha.50', 'whiteAlpha.50')(props),
+          borderRadius: '6px',
+          width: '6px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: mode('blackAlpha.200', 'whiteAlpha.200')(props),
+          borderRadius: '6px',
+        },
+      },
+    }),
+  },
+})
