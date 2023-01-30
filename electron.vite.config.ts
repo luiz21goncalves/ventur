b68e9f6e20591ccd2eedb0aa4039244fd76fa9f5
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-30 13:00:45 -0300

    chore: add config for tsconfigPaths

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,26 +1,32 @@
-import { resolve } from 'node:path'
+import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import jotaiPluginDebugLabel from 'jotai/babel/plugin-debug-label'
 import jotaiPluginReactRefresh from 'jotai/babel/plugin-react-refresh'
+import tsconfigPlugin from 'vite-tsconfig-paths'
+
+const tsconfigPaths = tsconfigPlugin({
+  projects: [path.resolve('tsconfig.json')],
+})
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [tsconfigPaths, externalizeDepsPlugin()],
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [tsconfigPaths, externalizeDepsPlugin()],
   },
   renderer: {
     plugins: [
+      tsconfigPaths,
       react({
         babel: { plugins: [jotaiPluginDebugLabel, jotaiPluginReactRefresh] },
       }),
     ],
     resolve: {
       alias: {
-        '@': resolve('src/'),
+        '@': path.resolve('src/'),
       },
     },
   },
