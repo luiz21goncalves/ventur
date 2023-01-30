905fd31846d91e959572dd8d4a0d5aa4a1d400fe
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-30 15:54:58 -0300

    chore: add react-query provider

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,14 +1,21 @@
 import './lib/dayjs'
 
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+import { queryClient } from './lib/query-client'
 import { Routes } from './Routes'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <ChakraProvider theme={defaultTheme}>
-      <Routes />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={defaultTheme}>
+        <Routes />
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
