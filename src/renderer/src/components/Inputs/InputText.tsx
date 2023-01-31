f757b01fc3a703bc652e40e6aabebfb68df4c9d5
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-31 16:45:28 -0300

    fix: ref in InputText

--- src/renderer/src/components/Inputs/InputText.tsx
+++ src/renderer/src/components/Inputs/InputText.tsx
@@ -1,16 +1,22 @@
 import { FormControl, FormLabel, Input, InputProps } from '@chakra-ui/react'
+import { ElementRef, forwardRef } from 'react'
 
 type InputTextProps = InputProps & {
   label: string
+  errorMessage?: string
 }
 
-export function InputText(props: InputTextProps) {
-  const { label, isRequired = true, ...attrs } = props
+export const InputText = forwardRef<ElementRef<typeof Input>, InputTextProps>(
+  (props, ref) => {
+    const { label, isRequired = true, errorMessage, ...attrs } = props
 
-  return (
-    <FormControl isRequired={isRequired}>
-      <FormLabel>{label}</FormLabel>
-      <Input {...attrs} />
-    </FormControl>
-  )
-}
+    return (
+      <FormControl isRequired={isRequired}>
+        <FormLabel>{label}</FormLabel>
+        <Input ref={ref} {...attrs} />
+      </FormControl>
+    )
+  },
+)
+
+InputText.displayName = 'InputText'
