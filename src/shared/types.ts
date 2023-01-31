b8ee270f5b4059fe52da3a7becc31df95d9cc7ce
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-31 10:07:47 -0300

    refactor: split home screen into small components

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -3,3 +3,14 @@ export type Holiday = {
   name: string
   date: string
 }
+
+export type Student = {
+  id: string
+  name: string
+  email?: string
+  password?: string
+  classes_per_week: number
+  classes_per_month: number
+  price_per_month: string
+  weekdays: number[]
+}
