7e112d9f93c90a8302c212e8b56819fa4c38810e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-31 10:34:31 -0300

    feat: add calendar layout

--- src/renderer/src/screens/Home/index.tsx
+++ src/renderer/src/screens/Home/index.tsx
@@ -1,27 +1,11 @@
-import { Grid, GridItem, useToken } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 
-import { Calendar } from '../../components/Calendar'
 import { StudentList } from './StudentList'
 
 export function Home() {
-  const [firstColumWidth, secondColumWith] = useToken('sizes', [
-    'xs',
-    'container.sm',
-  ])
-
   return (
-    <Grid
-      gridTemplateColumns={`${firstColumWidth} ${secondColumWith}`}
-      gap="4"
-      h="container.md"
-    >
-      <GridItem overflowY="auto" px="2" py="4">
-        <StudentList />
-      </GridItem>
-
-      <GridItem>
-        <Calendar />
-      </GridItem>
-    </Grid>
+    <Box overflowY="auto" px="2" py="4">
+      <StudentList />
+    </Box>
   )
 }
