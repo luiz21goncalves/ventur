44df3e4dc9eb7444f877b5b2fe9ac17698208bfc
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-03 17:01:57 -0300

    feat: add error message on input text

--- src/renderer/src/components/Inputs/InputText.tsx
+++ src/renderer/src/components/Inputs/InputText.tsx
@@ -1,4 +1,13 @@
-import { FormControl, FormLabel, Input, InputProps } from '@chakra-ui/react'
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  InputProps,
+  Text,
+  useColorModeValue,
+  useToken,
+} from '@chakra-ui/react'
+import { Asterisk } from 'phosphor-react'
 import { ElementRef, forwardRef } from 'react'
 
 type InputTextProps = InputProps & {
@@ -10,10 +19,21 @@ export const InputText = forwardRef<ElementRef<typeof Input>, InputTextProps>(
   (props, ref) => {
     const { label, isRequired = true, errorMessage, ...attrs } = props
 
+    const [red1, red2] = useToken('colors', ['red.500', 'red.400'])
+    const asteriskColor = useColorModeValue(red1, red2)
+
     return (
-      <FormControl isRequired={isRequired}>
-        <FormLabel>{label}</FormLabel>
+      <FormControl>
+        <FormLabel display="flex" alignItems="center" gap="px">
+          {label}
+          {isRequired && (
+            <Asterisk weight="bold" size={10} color={asteriskColor} />
+          )}
+        </FormLabel>
         <Input ref={ref} {...attrs} />
+        <Text textColor="gray" fontSize="sm" mt="0.5">
+          {errorMessage}
+        </Text>
       </FormControl>
     )
   },
