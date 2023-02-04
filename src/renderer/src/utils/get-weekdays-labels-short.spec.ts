44d2c06dc6ce9176921a999583342c72c347d90a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-04 13:06:34 -0300

    chore: add function to get weekdays labels

---
+++ src/renderer/src/utils/get-weekdays-labels-short.spec.ts
@@ -0,0 +1,45 @@
+import { describe, it } from 'vitest'
+
+import { getWeekdaysLabelsShort } from './get-weekdays-labels-short'
+
+describe('getWeekdaysLabels', () => {
+  it('should be able to return weekday labels', () => {
+    const weekdaysLabels = getWeekdaysLabelsShort([6, 5, 4, 3, 2, 1, 0])
+
+    expect(weekdaysLabels).toHaveLength(7)
+    expect(weekdaysLabels).toStrictEqual([
+      'sáb',
+      'sex',
+      'qui',
+      'qua',
+      'ter',
+      'seg',
+      'dom',
+    ])
+  })
+
+  it('should not be able to return weekday labels when receiving an array containing more than 7 positions', () => {
+    const weekdaysLabels = getWeekdaysLabelsShort(
+      Array.from({ length: 8 }).map((_, index) => index),
+    )
+
+    expect(weekdaysLabels).toHaveLength(0)
+    expect(weekdaysLabels).toStrictEqual([])
+  })
+
+  it('should not be able to return weekday labels when receiving an array containing an invalid weekday index', () => {
+    const weekdaysLabels = getWeekdaysLabelsShort([7, 8, 9, 10, 11, 12, 13])
+
+    expect(weekdaysLabels).toHaveLength(0)
+    expect(weekdaysLabels).toStrictEqual([])
+  })
+
+  it('should not be able to return weekday labels when receiving an array containing a one invalid index', () => {
+    const weekdaysLabels = getWeekdaysLabelsShort([1, 3, 5, 8])
+
+    console.log(weekdaysLabels)
+
+    expect(weekdaysLabels).toHaveLength(0)
+    expect(weekdaysLabels).toStrictEqual([])
+  })
+})
