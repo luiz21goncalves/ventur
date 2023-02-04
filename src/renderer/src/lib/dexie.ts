35f141447f2117effa03b1856a271bf443b1c2a2
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-04 09:13:08 -0300

    chore: add students schema

--- src/renderer/src/lib/dexie.ts
+++ src/renderer/src/lib/dexie.ts
@@ -1,16 +1,19 @@
 import Dexie, { Table } from 'dexie'
 
-import { Holiday } from '@/shared/types'
+import { Holiday, Student } from '@/shared/types'
 
 class VenturDexie extends Dexie {
   holidays!: Table<Holiday>
+  students!: Table<Student>
 
   constructor() {
     // TODO: rename this db
     super('test')
 
-    this.version(1).stores({
+    this.version(2).stores({
       holidays: '++id, name, date',
+      students:
+        '++id, name, birthdate, classes_per_week, price_per_month, *weekdays',
     })
   }
 }
