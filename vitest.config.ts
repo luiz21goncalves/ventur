2dddf9e2fd15758eb3a4a4ac6270d20150ae6fe5
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-05 09:25:40 -0300

    chore: update coverage files

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
 import tsconfigPlugin from 'vite-tsconfig-paths'
-import { defaultExclude, defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 
 const tsconfigPaths = tsconfigPlugin({
   projects: [path.resolve('tsconfig.json')],
@@ -13,7 +13,15 @@ export default defineConfig({
   test: {
     coverage: {
       all: true,
-      exclude: [...defaultExclude, 'build', 'out'],
+      exclude: [
+        'build',
+        'out',
+        'src/main',
+        'src/preload',
+        'electron.vite.config.ts',
+        'vitest.config.ts',
+        'vitest.setup.ts',
+      ],
     },
     environment: 'jsdom',
     globals: true,
