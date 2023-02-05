44bf7f3885529e2a725d27d89835bc365b24c00f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-05 10:00:29 -0300

    feat: apply calendar layout on student details route

--- src/renderer/src/screens/StudentDetails/index.tsx
+++ src/renderer/src/screens/StudentDetails/index.tsx
@@ -1,14 +1,5 @@
-import {
-  Center,
-  Flex,
-  Heading,
-  HStack,
-  SimpleGrid,
-  Text,
-  VStack,
-} from '@chakra-ui/react'
+import { Flex, Heading, Text, VStack } from '@chakra-ui/react'
 
-import { InputDate } from '../../components/Inputs/InputDate'
 import { useStudentDetailsQuery } from '../../queries/useStudentDetailsQuery'
 import { capitalize } from '../../utils/capitalize'
 import { formatMonetary } from '../../utils/format-monetary'
@@ -31,70 +22,29 @@ export function StudentDetails() {
 
   return (
     <Flex flexDir="column" gap="4">
-      <Heading mb="4">Aluno: {student?.name}</Heading>
+      <Heading mb="4">{student?.name}</Heading>
 
-      <VStack alignItems="flex-start" mb="8">
-        <Text>Aniversário: {student?.classes_per_week}</Text>
+      <VStack alignItems="flex-start">
         <Text>Aulas por semana: {student?.classes_per_week}</Text>
         <Text>Dias da aula: {weekdayLabels}</Text>
         <Text>Preço mensal: {formattedPrice}</Text>
+        {student?.birthdate && <Text>Aniversário: {student?.birthdate}</Text>}
+        <Text fontWeight="bold">Total de aulas: 20</Text>
+        <Text fontWeight="bold">A pagar: 200,00</Text>
       </VStack>
 
-      <Center>
-        <InputDate />
-      </Center>
+      <VStack h="80" overflowY="scroll" pr="2" alignItems="flex-start">
+        {Array.from({ length: 20 }).map((_, index) => {
+          const lineNumber = index + 1
+          const date = new Date().toDateString()
 
-      <SimpleGrid columns={2} gap="8" w="full">
-        <VStack>
-          <Heading size="md" mb="2">
-            Dezembro
-          </Heading>
-          <HStack alignItems="flex-start" gap="4">
-            <VStack justifySelf="flex-start" alignItems="flex-start">
-              <Text fontSize="lg">Total de aulas: 20</Text>
-              <Text fontSize="lg">A pagar: 200,00</Text>
-            </VStack>
-
-            <VStack h="80" overflowY="scroll" pr="2">
-              {Array.from({ length: 20 }).map((_, index) => {
-                const lineNumber = index + 1
-                const date = new Date().toDateString()
-
-                return (
-                  <Text key={lineNumber}>
-                    {lineNumber}: {date}
-                  </Text>
-                )
-              })}
-            </VStack>
-          </HStack>
-        </VStack>
-
-        <VStack>
-          <Heading size="md" mb="2">
-            Janeiro
-          </Heading>
-          <HStack alignItems="flex-start" gap="4">
-            <VStack justifySelf="flex-start" alignItems="flex-start">
-              <Text fontSize="lg">Total de aulas: 20</Text>
-              <Text fontSize="lg">A pagar: 200,00</Text>
-            </VStack>
-
-            <VStack h="80" overflowY="scroll" pr="2">
-              {Array.from({ length: 20 }).map((_, index) => {
-                const lineNumber = index + 1
-                const date = new Date().toDateString()
-
-                return (
-                  <Text key={lineNumber}>
-                    {lineNumber}: {date}
-                  </Text>
-                )
-              })}
-            </VStack>
-          </HStack>
-        </VStack>
-      </SimpleGrid>
+          return (
+            <Text key={lineNumber}>
+              {lineNumber}: {date}
+            </Text>
+          )
+        })}
+      </VStack>
     </Flex>
   )
 }
