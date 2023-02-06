626a2ffda4f5cf1bb710f6e8dc26d870ad8789b4
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-06 18:44:59 -0300

    chore: delete calendar layout

--- src/renderer/src/Routes.tsx
+++ src/renderer/src/Routes.tsx
@@ -2,7 +2,6 @@ import { Route, Router } from 'electron-router-dom'
 
 import { ROUTES } from '@/shared/routes'
 
-import { CalendarLayout } from './components/Layouts/CalendarLayout'
 import { DefaultLayout } from './components/Layouts/DefaultLayout'
 import { Home } from './screens/Home'
 
@@ -11,7 +10,7 @@ export function Routes() {
     <Router
       main={
         <Route path={ROUTES.HOME} element={<DefaultLayout />}>
-          <Route path={ROUTES.HOME} element={<CalendarLayout />}>
+          <Route path={ROUTES.HOME}>
             <Route path={ROUTES.HOME} element={<Home />} />
           </Route>
         </Route>
--- src/renderer/src/screens/Home/index.tsx
+++ src/renderer/src/screens/Home/index.tsx
@@ -1,11 +1,20 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Grid, GridItem } from '@chakra-ui/react'
 
+import { Calendar } from '../../components/Calendar'
 import { StudentList } from './StudentList'
 
 export function Home() {
   return (
-    <Box overflowY="auto" px="2" py="4">
-      <StudentList />
-    </Box>
+    <Grid gridTemplateColumns="repeat(2,1fr)" gap="4" h="container.md">
+      <GridItem overflowY="auto" px="2" py="4">
+        <Box overflowY="auto" px="2" py="4">
+          <StudentList />
+        </Box>
+      </GridItem>
+
+      <GridItem>
+        <Calendar />
+      </GridItem>
+    </Grid>
   )
 }
