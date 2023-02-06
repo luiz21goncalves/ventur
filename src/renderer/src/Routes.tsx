bc1ef3204c227245e2d3ae5575013f790ff35d14
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-06 18:41:41 -0300

    chore: remove routes all except the home route

--- src/renderer/src/Routes.tsx
+++ src/renderer/src/Routes.tsx
@@ -4,11 +4,7 @@ import { ROUTES } from '@/shared/routes'
 
 import { CalendarLayout } from './components/Layouts/CalendarLayout'
 import { DefaultLayout } from './components/Layouts/DefaultLayout'
-import { AttendanceList } from './screens/AttendanceList'
-import { CreateStudent } from './screens/CreateStudent'
-import { Holidays } from './screens/Holidays'
 import { Home } from './screens/Home'
-import { StudentDetails } from './screens/StudentDetails'
 
 export function Routes() {
   return (
@@ -17,15 +13,7 @@ export function Routes() {
         <Route path={ROUTES.HOME} element={<DefaultLayout />}>
           <Route path={ROUTES.HOME} element={<CalendarLayout />}>
             <Route path={ROUTES.HOME} element={<Home />} />
-            <Route path={ROUTES.HOLIDAYS} element={<Holidays />} />
-            <Route path={ROUTES.ATTENDANCE_LIST} element={<AttendanceList />} />
-            <Route
-              path={ROUTES.STUDENTS.DETAILS}
-              element={<StudentDetails />}
-            />
           </Route>
-
-          <Route path={ROUTES.STUDENTS.CREATE} element={<CreateStudent />} />
         </Route>
       }
     />
