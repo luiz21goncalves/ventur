31a4701ac8ed97fd1d11d248d813995a8d6dcf7b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-07 13:49:46 -0300

    feat: add missing empty messages

--- src/renderer/src/screens/Home/DeleteStudentModal.tsx
+++ src/renderer/src/screens/Home/DeleteStudentModal.tsx
@@ -1,6 +1,7 @@
 import { Divider, useDisclosure } from '@chakra-ui/react'
 import { Fragment } from 'react'
 
+import { EmptyMessage } from '../../components/EmptyMessage'
 import * as Modal from '../../components/Modal'
 import { useStudentsQuery } from '../../queries/useStudentsQuery'
 import { DeleteStudentForm } from './DeleteStudentForm'
@@ -9,6 +10,8 @@ export function DeleteStudentModal() {
   const { isOpen, onClose, onOpen } = useDisclosure()
 
   const { data: students } = useStudentsQuery()
+
+  const isEmptyList = students?.length === 0
 
   return (
     <>
@@ -21,16 +24,20 @@ export function DeleteStudentModal() {
           <Modal.Header>Remover alunos</Modal.Header>
 
           <Modal.Body mb="8" display="flex" flexDir="column" gap="2">
-            {students?.map((student, index, original) => {
-              const isNotLastItem = original.length > index + 1
+            {isEmptyList ? (
+              <EmptyMessage>Não há alunos.</EmptyMessage>
+            ) : (
+              students?.map((student, index, original) => {
+                const isNotLastItem = original.length > index + 1
 
-              return (
-                <Fragment key={student.id}>
-                  <DeleteStudentForm student={student} />
-                  {isNotLastItem && <Divider />}
-                </Fragment>
-              )
-            })}
+                return (
+                  <Fragment key={student.id}>
+                    <DeleteStudentForm student={student} />
+                    {isNotLastItem && <Divider />}
+                  </Fragment>
+                )
+              })
+            )}
           </Modal.Body>
         </Modal.Content>
       </Modal.Root>
